6a12058356e76697a633762205c10de5d896476e
Doug Williamson <douglasmwilliamson@gmail.com>
2025-02-14 16:58:43 -0500

    Updates for adding a reusable loading container for buttons

--- projects/ng-rhombus-admin/src/app/app.store.ts
+++ projects/ng-rhombus-admin/src/app/app.store.ts
@@ -1,8 +1,10 @@
 import { inject } from "@angular/core";
-import { NavigationStart, NavigationEnd, NavigationError, Router, ActivationStart, ActivationEnd } from "@angular/router";
+import { NavigationStart, NavigationEnd, Router } from "@angular/router";
 import { patchState, signalStore, withHooks, withMethods, withState } from "@ngrx/signals";
 import { rxMethod } from '@ngrx/signals/rxjs-interop';
 import { pipe, tap } from "rxjs";
+import { NgRhombusAdminAuthenticationService } from "./services/auth.service";
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 type AppState = {
     loading: boolean;
@@ -16,7 +18,36 @@ export const NgRhombusAdminAppStore = signalStore(
     { providedIn: 'root' },
     withState(initialState),
     withMethods(
-        (store) => ({
+        (
+            store,
+            authService = inject(NgRhombusAdminAuthenticationService),
+            router = inject(Router),
+            snackbar = inject(MatSnackBar)
+        ) => ({
+            login: async (email: string, password: string) => {
+                if (!email || !password) {
+                    return;
+                }
+
+                try {
+                    patchState(store, { loading: true });
+                    await authService.login(email, password);
+                    router.navigate(['/']);
+                } catch (error) {
+                    snackbar.open('Invalid email or password', 'Close', {
+                        verticalPosition: 'top',
+                        horizontalPosition: 'right',
+                    });
+                } finally {
+                    setTimeout(() => {
+                        patchState(store, { loading: false });
+                    }, 1000)
+                }
+            },
+            logout: async () => {
+                await authService.logout();
+                router.navigate(['/login']);
+            },
             startLoading: () => {
                 patchState(store, { loading: true });
             },
--- projects/ng-rhombus/src/lib/shell/services/authentication.service.ts
+++ projects/ng-rhombus/src/lib/shell/services/authentication.service.ts
@@ -1,26 +1,18 @@
 import { inject, Injectable } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { Auth, authState, signInWithEmailAndPassword } from '@angular/fire/auth';
-import { Firestore } from '@angular/fire/firestore';
 import { from, Observable, of } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
-
-export interface AuthenticationResponse {
-	token: string;
-	expiration: string;
-}
 
 @Injectable({
 	providedIn: 'root'
 })
 export class NgRhombusAuthenticationService {
-
-	firestore = inject(Firestore);
 	firebaseAuth = inject(Auth);
 	currentUser$ = authState(this.firebaseAuth);
 
-	login(email: string, password: string): Observable<void> {
-		return from(signInWithEmailAndPassword(this.firebaseAuth, email, password).then(() => { }));
+	login(email: string, password: string): Promise<void> {
+		return signInWithEmailAndPassword(this.firebaseAuth, email, password).then(() => { });
 	}
 
 	logout(): Promise<any> { return this.firebaseAuth.signOut(); }

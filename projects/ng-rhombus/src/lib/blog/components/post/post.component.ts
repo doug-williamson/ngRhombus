89a5546b8856971df509c3a350c730a6a0d0fe00
Doug Williamson <douglasmwilliamson@gmail.com>
2026-01-05 11:33:26 -0500

    Added FontAwesome, added margins to app-views

--- projects/ng-rhombus/src/lib/blog/components/post/post.component.ts
+++ projects/ng-rhombus/src/lib/blog/components/post/post.component.ts
@@ -2,12 +2,12 @@ import { Component, inject, input, OnInit, signal } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { IBlog } from '../../models/blog';
 import { MarkdownComponent } from 'ngx-markdown';
-import { NgClass } from '@angular/common';
+import { DatePipe, NgClass } from '@angular/common';
 import { ThemeService } from '../../../shell/services/theme.service';
 
 @Component({
   selector: 'ng-rhombus-blog-post',
-  imports: [NgClass, MarkdownComponent, MatCardModule],
+  imports: [DatePipe, NgClass, MarkdownComponent, MatCardModule],
   templateUrl: './post.component.html',
   styleUrl: './post.component.scss'
 })
--- projects/ng-rhombus/src/lib/socials/list/list.component.ts
+++ projects/ng-rhombus/src/lib/socials/list/list.component.ts
@@ -1,27 +1,30 @@
 import { Component, input } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { faGithub, faLinkedin, faYoutube, faInstagram, faTwitch, faTiktok, faBluesky } from '@fortawesome/free-brands-svg-icons';
+import { faCloud, faLink } from '@fortawesome/free-solid-svg-icons';
 import { ISocial, SocialsSource } from '../socials.service';
 
 @Component({
   selector: 'ng-rhombus-socials-list',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule],
+  imports: [FontAwesomeModule, MatToolbarModule, MatIconModule],
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
 export class NgRhombusSocialsListComponent {
   socials = input<ISocial[]>([]);
 
-  // Map source to Material icon names (adjust as desired)
-  iconFor(source?: SocialsSource): string {
+  iconFor(source?: SocialsSource): IconDefinition {
     switch (source) {
-      case SocialsSource.Twitch: return 'sports_esports';
-      case SocialsSource.YouTube: return 'ondemand_video';
-      case SocialsSource.Instagram: return 'camera_alt';
-      case SocialsSource.TikTok: return 'music_note';
-      case SocialsSource.Bluesky: return 'cloud';
-      default: return 'link';
+      case SocialsSource.YouTube: return faYoutube;
+      case SocialsSource.Instagram: return faInstagram;
+      case SocialsSource.Twitch: return faTwitch;
+      case SocialsSource.TikTok: return faTiktok;
+      case SocialsSource.Bluesky: return faBluesky; // no official brand, fallback
+      default: return faLink;
     }
   }
 
